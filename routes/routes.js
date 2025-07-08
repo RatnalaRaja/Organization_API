998d818669256a11c4cf342a277151719bfeac2d
Ratnala Raja <rk635238@gmail.com>
2025-07-08 11:59:13 +0530

    Updated_Routes

--- routes/routes.js
+++ routes/routes.js
@@ -11,9 +11,10 @@ const generateToken = (org) => {
   });
 };
 
-router.get("/org", protect,async (req, res) => {
+router.get("/org", async (req, res) => {
   try {
-    const orgs = await Organization.findAll();
+    //const orgs = await Organization.findAll();
+    const [orgs,metadata] = await sequelize.query("select org_name,org_id from Organizations");
     res.status(200).json(orgs);
   } catch (error) {
     console.error("Error fetching organizations:", error);
@@ -28,8 +29,8 @@ router.post("/org", async (req, res) => {
       return res.status(400).json("All fields are required");
     }
     const orgs = await Organization.create({ org_name, org_id });
-    const token = generateToken(orgs);
-    console.log(token);
+    //const token = generateToken(orgs);
+    //console.log(token);
     res.status(201).json({
       message: "Org Created Successfully",
       org: orgs,
@@ -41,19 +42,20 @@ router.post("/org", async (req, res) => {
   }
 });
 
-router.put('/org/:id', async (req, res) => {
-  try {
-    const orgId = await Organization.findByPk(req.params.id);
-    if (orgId) {
-      await orgId.update(req.body);
-      res.json({ message: "Organization Updated", org: orgId });
-    } else {
-      res.status(404).json("Organization not found");
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+// router.put('/org/:id', async (req, res) => {
+//   try {
+//     const orgId = await Organization.findByPk(req.params.id);
+   
+//     if (orgId) {
+//       await orgId.update(req.body);
+//       res.json({ message: "Organization Updated", org: orgId });
+//     } else {
+//       res.status(404).json("Organization not found");
+//     }
+//   } catch (error) {
+//     res.status(400).json({ error: error.message });
+//   }
+// });
 
 
 router.delete('/org/:id', async (req, res) => {
@@ -71,3 +73,38 @@ router.delete('/org/:id', async (req, res) => {
 });
 
 module.exports = router;
+
+
+router.put('/org/:id', async (req, res) => {
+  try {
+    const orgId = req.params.id;
+    const { org_name } = req.body;
+
+    
+    const [org] = await sequelize.query(
+      "SELECT * FROM organizations WHERE org_id = :id",
+      {
+        replacements: { id: orgId },
+        type: sequelize.QueryTypes.SELECT
+      }
+    );
+
+    if (!org) {
+      return res.status(404).json({ message: "Organization not found" });
+    }
+
+  
+    await sequelize.query(
+      "UPDATE organizations SET org_name = :org_name WHERE org_id = :id",
+      {
+        replacements: {org_name,id: orgId },
+        type: sequelize.QueryTypes.UPDATE
+      }
+    );
+
+    res.json({ message: "Organization updated successfully" });
+
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
